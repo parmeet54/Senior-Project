10d2e9e1611d4d9e07c9da368a9c9074351dfdc7
derian931 <deventura95@hotmail.com>
2020-05-05 23:31:53 -0700

    Handles showing notifications in a card format

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typorgraphy from "@material-ui/core/Typography";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
+const styles = {
+  card: {
+    display: "flex",
+    marginBottom: 20,
+    minWidth: 500,
+    maxWidth: 501,
+    minHeight: 150,
+    maxHeight: 151,
+    borderRadius: 12,
+  },
+  content: {
+    padding: 15,
+    objectFit: "cover",
+  },
+};
 class Notifications extends Component {
   state = {};
   render() {
+    dayjs.extend(relativeTime);
     const {
+      classes,
       notifications: {
         getter,
         sender,
@@ -16,13 +39,21 @@ class Notifications extends Component {
     } = this.props;
     //console.log();
     return (
-      <div>
-        <p>
-          notification: {notificationID}: (from {sender} to {getter})
-        </p>
-      </div>
+      <Card className={classes.card}>
+      <CardContent className={classes.content}>
+        <Typorgraphy variant="h5">
+          Notification ID:{notificationID}
+        </Typorgraphy>      
+        <Typorgraphy variant="body1" color="textSecondary">
+            {dayjs(createdAt).fromNow()}
+        </Typorgraphy>
+        <Typorgraphy variant="body1" color="textPrimary">
+            {sender} has decided to {type} with you
+        </Typorgraphy>       
+      </CardContent>
+    </Card>
     );
   }
 }
 
-export default Notifications;
+export default withStyles(styles)(Notifications);
